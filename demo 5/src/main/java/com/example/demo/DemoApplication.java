648e1fb5c78127505aaec3d6d928ddaddb4c3aa5
Bardia Habibkhoda <bhab2@student.monash.edu>
2021-09-05 02:38:57 +1000

    V6: Added ability to decode messages on temporary socket. Work in progress; currently accepts 16 bit binary input message and just displays it

--- demo 5/src/main/java/com/example/demo/DemoApplication.java
+++ demo 5/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 import javax.management.RuntimeErrorException;
+import javax.websocket.Decoder.Binary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.json.JSONObject;
 import org.json.JSONString;
 
+enum MessageType {
+	init, temperature, humidity, pressure
+}
 
 
 @SpringBootApplication
@@ -90,6 +94,69 @@ public class DemoApplication {
 		}
 		return "REALLY TERRIBLY failed";
 		
+	}
+
+	@GetMapping("/tmpsckt1")
+	public String ingresString(@RequestParam(value = "binMsg", defaultValue = "0000111100000000") String binaryMsg) {
+
+		// This method is supposed to take a msg and push it to an SQL server
+
+		// This part is the decoding.
+		
+		// Error handling
+
+		if (binaryMsg == "") {
+			// move to an "Actually empty" function later
+			return "Your message device sent nothing.";
+		}
+		if (binaryMsg.length() < 16) {
+			// The message has to be 16 bits. Support for larger msgs coming soon
+			return "Message needs to be 16 bits. Check with RPI portral.";
+		}
+		try {
+			int temporaryInt = Integer.parseInt(binaryMsg, 2);
+		}
+		catch (Exception e) {
+			// Do nothing
+			return "You cannot input non-binary messages.";
+		}
+
+		// Good. Nothing to worry about.
+		
+		// Decode values from message
+		String deviceIdString = binaryMsg.substring(0,4);
+		String msgTypeString = binaryMsg.substring(4,8);
+		String msgValueString = binaryMsg.substring(8,16);
+
+		// Map to Enums
+		int msgTypeInt = Integer.parseInt(msgTypeString,2);
+		MessageType messageType = MessageType.init;
+		switch (msgTypeInt) {
+			case 1110:
+				messageType = MessageType.temperature;
+				break;
+			case 1100:
+				messageType = MessageType.humidity;
+				break;
+			default:
+				messageType = MessageType.pressure;
+		}
+
+		int decimalMsgValue = Integer.parseInt(msgValueString,2);
+
+
+		// Just testing for now
+		return ( "Your device id is: " + deviceIdString + "\nYour message type was: " + messageType + "\nYour message value was: " + decimalMsgValue);
+
+		// Try to push the id into 
+
+		// SQL PUSH SECTION //
+
+		// TODO //
+
+		// END //
+
+
 	}
 
 }
